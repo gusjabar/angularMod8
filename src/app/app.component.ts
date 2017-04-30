aca05eef7999ad97e1c29778244ffb974872e8e3
Gustavo <jg_barreto@hotmail.com>
2017-04-29 21:53:05 -0400

    creating observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Angular for beginners: Module VIII';
+  elementRef: ElementRef;
+  ngOnInit() {
+    var keyups = Observable.fromEvent($('#search'), 'keyup');
+    keyups.subscribe(data => console.log(data));
+  }
+  constructor(elementRef: ElementRef) {
+    this.elementRef = elementRef;
+  }
 }
